8f3a9a7947207d979df90e75a09563276d408957
Sven Lito <me@svenlito.com>
2013-05-16 22:21:05 +0100

    [wip] first commit

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+/*global module:false*/
+
+module.exports = function(grunt) {
+
+  "use strict";
+
+  // Project configuration.
+  grunt.initConfig({
+
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'index.js',
+        'lib/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    mochaTest: {
+      all: [ 'test/**/*.test.js' ],
+    },
+
+    mochaTestConfig: {
+      options: {
+        reporter: 'tap',
+        testing: 1,
+        node_env: 'test',
+      }
+    },
+
+    watch: {
+      files: '**/*',
+      tasks: ['jshint', 'mochaTest']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'mochaTest'] );
+
+};
+
---
+++ server/server.js
@@ -0,0 +1,64 @@
+//
+// # server/server
+//
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+var express   = require('express'),
+    resource  = require('express-resource'),
+    fs        = require('fs'),
+    _         = require('lodash'),
+    app       = module.exports = express(),
+    path      = require('path'),
+    config    = require('konphyg')(path.normalize(__dirname + '/../config/'));
+
+var setHeaders = require(path.normalize(__dirname + '/../lib/middleware/headers'));
+
+
+module.exports = function(opts) {
+
+  'use strict';
+
+  opts = _.extend({
+    port :      3333,
+    protocol : 'http',
+    baseDir :   path.normalize(__dirname + '/../')
+  }, opts || {});
+
+  //
+  // configure express middleware
+  //
+  app.configure(function() {
+    app.use(setHeaders());
+    app.use(express.compress());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(function(err, req, res, next){
+      console.error(err.stack);
+      res.send(500, 'Something broke!');
+    });
+  });
+
+  //
+  // setup environment
+  //
+  app.configure("development", function() {
+    app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+
+    // log verbosely
+    app.use(express.logger(({ format: ':method :status :url' })));
+  });
+
+  app.configure("production", function() {
+    app.use(express.errorHandler());
+  });
+
+  app.resource('packages', require(opts.baseDir + 'lib/controllers/packages'));
+
+  // Actually listen
+  app.listen(opts.port || null);
+  console.log("Serving at http://localhost:" + (opts.port || ''));
+};
