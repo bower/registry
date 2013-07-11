379d275cb172e234ff682fa7b1679a093dbf3757
Sven Lito <me@svenlito.com>
2013-07-11 20:27:19 +0100

    replace grunt-mocha-test with grunt-simple-mocha
    to be more in line with other bower org projects

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,11 +36,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-simple-mocha');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'mochaTest'] );
-  grunt.registerTask('test', ['jshint', 'mochaTest'] );
+  grunt.registerTask('default', ['jshint', 'simplemocha:full'] );
+  grunt.registerTask('test', ['jshint', 'simplemocha:full'] );
 
 };
 
