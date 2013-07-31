1fd0fdfc3a1a5105ebff1ef5c695dc78de5e0160
Sven Lito <me@svenlito.com>
2013-07-31 11:08:57 +0100

    adds object factories

--- test/support/factories.js
+++ test/support/factories.js
@@ -1,7 +1,26 @@
 module.exports = {
-  package: function (resource, name, url) {
-    this.resource = resource || 'package';
-    this.name = name || 'default';
-    this.url = url || 'default';
+
+  package: function (name, description, keywords, owners, type, url, versions) {
+    this.name = name || '';
+    this.description = description || '',
+    this.keywords = keywords || [];
+    this.owners = owners || [];
+    this.type = type || '';
+    this.url = url || '';
+    this.versions = versions || [];
+  },
+
+  user: function (name, email, url, packages) {
+    this.name = name || '';
+    this.email = email || '';
+    this.url = url || '';
+    this.packages = packages || [];
+  },
+
+  info: function (registry, name, description) {
+    this.registry = registry || '';
+    this.name = name || '';
+    this.description = description || '';
   }
+
 };
