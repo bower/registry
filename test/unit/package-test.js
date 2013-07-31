72ba3442617cf8c64dd0fc9f25dee4bff591a38f
Sven Lito <me@svenlito.com>
2013-07-31 20:58:36 +0100

    no more single letter vars

--- test/unit/package-test.js
+++ test/unit/package-test.js
@@ -25,18 +25,18 @@ describe('Package', function () {
   describe('Model', function () {
 
     beforeEach(function () {
-      this.p = new Package(registry, mockData);
+      this.pkg = new Package(registry, mockData);
     });
 
     describe('Constructor', function () {
 
       it('should be an instance of Package', function () {
-        expect(this.p).to.be.a(Package);
+        expect(this.pkg).to.be.a(Package);
       });
 
       it('should have normal exposed props', function () {
-        expect(this.p.registry).to.eql(registry);
-        expect(this.p._model).to.be.a(Object);
+        expect(this.pkg.registry).to.eql(registry);
+        expect(this.pkg._model).to.be.a(Object);
       });
 
     });
@@ -44,9 +44,9 @@ describe('Package', function () {
     describe('Property initialization and retrieval', function () {
 
       it('should happen on construction', function () {
-        expect(this.p.get('name')).to.eql(mockData.name);
-        expect(this.p.get('version')).to.eql(mockData.version);
-        expect(this.p.get('url')).to.eql(mockData.url);
+        expect(this.pkg.get('name')).to.eql(mockData.name);
+        expect(this.pkg.get('version')).to.eql(mockData.version);
+        expect(this.pkg.get('url')).to.eql(mockData.url);
       });
 
     });
@@ -54,8 +54,8 @@ describe('Package', function () {
     describe('Property assignment', function () {
 
       it('should work using .set()', function () {
-        this.p.set('main', ['file.js']);
-        expect(this.p.get('main')).to.eql(['file.js']);
+        this.pkg.set('main', ['file.js']);
+        expect(this.pkg.get('main')).to.eql(['file.js']);
       });
 
     });
@@ -63,15 +63,15 @@ describe('Package', function () {
     describe('toObject', function () {
 
       it('should have public properties', function () {
-        var obj = this.p.toObject();
+        var obj = this.pkg.toObject();
 
-        expect(obj.name).to.eql(this.p.get('name'));
-        expect(obj.version).to.eql(this.p.get('version'));
-        expect(obj.url).to.eql(this.p.get('url'));
+        expect(obj.name).to.eql(this.pkg.get('name'));
+        expect(obj.version).to.eql(this.pkg.get('version'));
+        expect(obj.url).to.eql(this.pkg.get('url'));
       });
 
       it('should not have private properties', function () {
-        var obj = this.p.toObject();
+        var obj = this.pkg.toObject();
         expect(obj.resource).to.be(undefined);
       });
 
@@ -80,8 +80,8 @@ describe('Package', function () {
     describe('toJSON', function () {
 
       it('should be the same as toObject', function () {
-        var obj = this.p.toObject();
-        var json = this.p.toJSON();
+        var obj = this.pkg.toObject();
+        var json = this.pkg.toJSON();
 
         expect(JSON.parse(json)).to.eql(obj);
       });
