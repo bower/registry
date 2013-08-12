cb9c3806039a65c9f51018e7ca68678440510b52
Sven Lito <me@svenlito.com>
2013-08-12 15:26:29 +0100

    set jshint settings on ddocs

--- couchapp/ddocs.js
+++ couchapp/ddocs.js
@@ -1,22 +1,24 @@
+/* global emit:true */
+
 var docs = [{
   _id: '_design/packages',
   views: {
     all: {
-      map: function(doc) {
+      map: function (doc) {
         if (doc.resource === 'packages') {
           emit(doc._id, doc);
         }
       }
     },
     byDate: {
-      map: function(doc) {
+      map: function (doc) {
         if (doc.resource === 'packages') {
           emit(doc.date, doc);
         }
       }
     },
     by_name: {
-      map: function(doc) {
+      map: function (doc) {
         if (doc.resource === 'packages') {
           emit(doc._id, doc);
         }
@@ -27,14 +29,14 @@ var docs = [{
   _id: '_design/users',
   views: {
     all: {
-      map: function(doc) {
+      map: function (doc) {
         if (doc.resource === 'users') {
           emit(doc._id, doc);
         }
       }
     },
     by_name: {
-      map: function(doc) {
+      map: function (doc) {
         if (doc.resource === 'users') {
           emit(doc._id, doc);
         }
