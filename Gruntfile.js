52bfd2f79a1563eaf77736a1cea744da14f07ea0
Mat Scales <mscales@google.com>
2013-09-30 16:50:26 +0100

    Added code coverage so we can see where we need more tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,16 +26,22 @@ module.exports = function (grunt) {
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint', 'simplemocha:full']
-        }
+        },
 
+        exec: {
+            coverage: {
+                command: 'node node_modules/istanbul/lib/cli.js cover --dir ./coverage node_modules/mocha/bin/_mocha -- -R dot test/**/*-test.js'
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-exec');
 
-    // Default task.
     grunt.registerTask('default', ['jshint', 'simplemocha:full']);
+    grunt.registerTask('coverage', 'exec:coverage');
     grunt.registerTask('test', ['jshint', 'simplemocha:full']);
 
 };
