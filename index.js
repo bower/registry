4273b83bde265fee7db36bb7f128d74cda2ac4a9
James Reggio <james.reggio@gmail.com>
2014-06-06 11:20:58 -0700

    Cleanup whitespace

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var express = require('express');
 var app = express();
 
 app.configure(function () {
-	app.use(express.logger());
-	app.use(express.compress());
-	app.use(express.bodyParser());
-	app.use(app.router);
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.logger());
+    app.use(express.compress());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.listen(process.env.PORT);
