20e63ce953a6eeecf13e26289015128ae6f2cea3
Patrick Kettner <patrickkettner@gmail.com>
2014-07-20 05:17:17 -0400

    add package name validation
    fixes #59

---
+++ test/packageNames.js
@@ -0,0 +1,67 @@
+var assert = require('chai').assert;
+var validName = require('../lib/validName');
+
+
+describe('package names', function(){
+
+      it('should support simple names', function () {
+          return assert.isTrue(validName('jquery'));
+      });
+
+      it('should allow dots', function () {
+          return assert.isTrue(validName('jquery.is.the.coolest'));
+      });
+
+      it('should allow dashes', function () {
+          return assert.isTrue(validName('jquery-is-the-coolest'));
+      });
+
+      it('should allow underscores', function () {
+          return assert.isTrue(validName('jquery_is_the_coolest'));
+      });
+
+      it('should not allow consecutive dots', function () {
+          return assert.isFalse(validName('jquery..is..the..coolest'));
+      });
+
+      it('should not allow consecutive dashes', function () {
+          return assert.isFalse(validName('jquery--is--the--coolest'));
+      });
+
+      it('should not allow consecutive underscores', function () {
+          return assert.isFalse(validName('jquery__is__the__coolest'));
+      });
+
+      it('should not allow dots at the begining', function () {
+          return assert.isFalse(validName('.jquery'));
+      });
+
+      it('should not allow dots at the end', function () {
+          return assert.isFalse(validName('jquery.'));
+      });
+
+      it('should not allow dashes at the begining', function () {
+          return assert.isFalse(validName('-jquery'));
+      });
+
+      it('should not allow dashes at the end', function () {
+          return assert.isFalse(validName('jquery-'));
+      });
+
+      it('should not allow underscores at the begining', function () {
+          return assert.isFalse(validName('_jquery'));
+      });
+
+      it('should not allow underscores at the end', function () {
+          return assert.isFalse(validName('jquery_'));
+      });
+
+      it('should not allow uppercase letters', function () {
+          return assert.isFalse(validName('jQuery'));
+      });
+
+      it('should not allow names longer than 50 chars ', function () {
+          return assert.isFalse(validName('thisisastringthatsoverfiftycharacterslongforsomereason'));
+      });
+
+});
