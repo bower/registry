8e1e96ab8ce7e7cbf317bb11a2e2c09b71a765b8
Adam Stankiewicz <sheerun@sher.pl>
2016-02-13 20:58:41 +0100

    Revert "Remove multer from project"
    This reverts commit 0a1be44b3a3f29624629ab326e4455fd92f73051.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV === 'production') {
 var express = require('express');
 var cors = require('./lib/cors');
 var config = require('config');
+var multer  = require('multer');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -24,6 +25,7 @@ app.use(morgan('combined', {
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: './uploads/'}));
 
 app.listen(config.get('port'));
 
