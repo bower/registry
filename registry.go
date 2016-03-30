7033941d2bebfb72b0934a8f174a02d880b6a99a
Adam Stankiewicz <sheerun@sher.pl>
2016-03-30 08:26:16 +0200

    Add go proxy server

---
+++ registry.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/elazarl/goproxy"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+
+	proxy.NonproxyHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		req.Host = "bower.herokuapp.com"
+		req.URL.Scheme = "http"
+		req.URL.Host = "bower.herokuapp.com"
+		proxy.ServeHTTP(w, req)
+	})
+
+	fmt.Println("Starting web server at port", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, proxy))
+}
