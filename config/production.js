8db80b07f3a23e94a1b4ccacbb62e7e3fe50a5dc
Adam Stankiewicz <sheerun@sher.pl>
2017-09-14 14:08:42 +0000

    Add purging of cloudflare cache

--- config/production.js
+++ config/production.js
@@ -7,5 +7,10 @@ module.exports = {
         servers: process.env.MEMCACHEDCLOUD_SERVERS.split(','),
         username: process.env.MEMCACHEDCLOUD_USERNAME,
         password: process.env.MEMCACHEDCLOUD_PASSWORD
+    },
+    cloudflare: {
+        email: process.env.CLOUDFLARE_EMAIL,
+        key: process.env.CLOUDFLARE_KEY,
+        zone: process.env.CLOUDFLARE_ZONE
     }
 };
