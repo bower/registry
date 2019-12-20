711fce6de57427e5ee2c579822a122c538a5df55
Adam Stankiewicz <sheerun@sher.pl>
2019-12-20 23:40:55 +0100

    Change default port to 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const MiniSearch = require('minisearch')
 const path = require('path')
 const url = require('url')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const packages = {}
 let totalPackages = 0
